5e72dd4e8fa6089e00af4832e1999b36fcc33d57
Victor Cordova <victorcordovapoma@gmail.com>
2025-11-05 14:17:39 -0500

    [UPDATE] taller: PartiManager

--- src/main/java/com/taller/PartiManager.java
+++ src/main/java/com/taller/PartiManager.java
@@ -1,26 +1,28 @@
 package com.taller;
 
 import com.models.Course;
+import com.models.Participation;
 import com.models.Student;
 import com.repository.CourseRepository;
 import com.repository.ParticipationRepository;
 import com.repository.StudentRepository;
 import java.util.ArrayList;
 import java.util.List;
-import participation.Participation;
-import utils.JsonHelper;
+
+// import utils.JsonHelper;
 
 public class PartiManager {
 
     private final int MAX_STUDENTS;
     private final List<Student> students;
+    private final List<Course> courses;
     private final List<Participation> participations;
     private final StudentRepository studentRepo;
     private final ParticipationRepository participationRepo;
     private final CourseRepository courseRepo;
 
-    private final String studentsFile = "data/students.json";
-    private final String participationsFile = "data/participations.json";
+    // private final String studentsFile = "data/students.json";
+    // private final String participationsFile = "data/participations.json";
 
     public PartiManager(int maxStudents) {
         this.MAX_STUDENTS = maxStudents;
@@ -28,12 +30,11 @@ public class PartiManager {
         this.participationRepo = new ParticipationRepository();
         this.courseRepo = new CourseRepository();
 
-        this.students = new ArrayList<>(
-            JsonHelper.readListFromFile(studentsFile, Student.class)
-        );
-        this.participations = new ArrayList<>(
-            JsonHelper.readListFromFile(participationsFile, Participation.class)
-        );
+        this.courses = new ArrayList<>();
+        this.students = new ArrayList<>();
+        // JsonHelper.readListFromFile(studentsFile, Student.class)
+        this.participations = new ArrayList<>();
+        // JsonHelper.readListFromFile(participationsFile, Participation.class)
     }
 
     private boolean isFull(List<Student> students) {
@@ -41,27 +42,28 @@ public class PartiManager {
     }
 
     public boolean registerStudent(String dni, String fullName) {
-        List<Student> students = studentRepo.getAllStudents();
+        // List<Student> students = studentRepo.getAllStudents();
 
         if (isFull(students)) {
             IO.println("Student list is full.");
             return false;
         }
 
-        if (studentRepo.getStudentByDni(dni)) {
+        if (studentRepo.getStudentByDni(dni, this.students) != null) {
             IO.println("Invalid DNI.");
             return false;
         }
-        if (studentRepo.fullNameAlreadyExist(fullName)) {
-            IO.println("Invalid Name.");
-            return false;
-        }
-        IO.println(
-            "full name exits?: " + studentRepo.fullNameAlreadyExist(fullName)
-        );
+        // if (studentRepo.fullNameAlreadyExist(fullName, this.students)) {
+        //     IO.println("Invalid Name.");
+        //     return false;
+        // }
+        // IO.println(
+        //     "full name exits?: " +
+        //         studentRepo.fullNameAlreadyExist(fullName, this.students)
+        // );
         Student student = new Student(dni, fullName);
         students.add(student);
-        studentRepo.saveAll(students);
+        // studentRepo.saveAll(students);
         return true;
     }
 
@@ -72,57 +74,64 @@ public class PartiManager {
     }
 
     public boolean registerParticipation(String studentDni) {
-        List<Student> students = studentRepo.getAllStudents();
-        List<Participation> participations = participationRepo.getAll();
+        // List<Student> students = studentRepo.getAllStudents();
+        // List<Participation> participations = participationRepo.getAll();
 
         // Student found = null;
 
-        Student student = findStudentByDni(students, studentDni);
+        Student student = studentRepo.getStudentByDni(
+            studentDni,
+            this.students
+        );
         if (student == null) {
             return false;
         }
-        Participation participation = new Participation(student);
+        Participation participation = new Participation(student.getUuid());
 
-        participations.add(participation);
-        participationRepo.saveAll(participations);
+        this.participations.add(participation);
+        // participationRepo.saveAll(participations);
 
         return true;
     }
 
     public void showParticipations() {
         List<Participation> participations = participationRepo.getAll();
+
         for (Participation item : participations) {
             IO.println(
                 "The student: " +
-                    item.getStudent().fullName +
+                    studentRepo.getStudentByUuid(
+                        item.getStudentUuid(),
+                        this.students
+                    ).fullName +
                     " has participated."
             );
         }
     }
 
     public boolean registerCourse(String name, String description) {
-        List<Course> courses = courseRepo.getAll();
+        // List<Course> courses = courseRepo.getAll();
         Course course = new Course(name, description);
 
-        courses.add(course);
-        courseRepo.saveAll(courses);
+        this.courses.add(course);
+        // courseRepo.saveAll(courses);
 
         return true;
     }
 
     public void showCourses() {
-        List<Course> courses = courseRepo.getAll();
-        for (Course item : courses) {
+        // List<Course> courses = courseRepo.getAll();
+        for (Course item : this.courses) {
             IO.print("Course: " + item.name + " ");
             IO.println("Description: " + item.description);
         }
     }
 
-    private Student findStudentByDni(List<Student> students, String dni) {
-        for (Student item : students) {
-            if (item.dni.equals(dni)) return item;
-        }
+    // private Student findStudentByDni(List<Student> students, String dni) {
+    //     for (Student item : students) {
+    //         if (item.dni.equals(dni)) return item;
+    //     }
 
-        return null;
-    }
+    //     return null;
+    // }
 }
