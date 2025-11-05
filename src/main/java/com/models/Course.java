669b47d5b7e34eb47f4d1478dc6b754fd52578cc
Victor Andres Cordova Poma <victorcordova@Mac-mini-de-Victor.local>
2025-11-05 14:15:10 -0500

    [UPDATE] models: Course, Student(implemented uuid)

--- src/main/java/com/models/Course.java
+++ src/main/java/com/models/Course.java
@@ -1,13 +1,20 @@
 package com.models;
 
+import java.util.UUID;
+
 public class Course {
 
+    private UUID uuid;
     public final String name;
     public final String description;
 
     public Course(String name, String description) {
-        // this.id = id;
+        this.uuid = UUID.randomUUID();
         this.name = name;
         this.description = description;
+    }
+
+    public UUID getUuid() {
+        return uuid;
     }
 }
--- src/main/java/com/models/Student.java
+++ src/main/java/com/models/Student.java
@@ -1,12 +1,20 @@
 package com.models;
 
+import java.util.UUID;
+
 public class Student {
 
+    private UUID uuid;
     public final String dni;
     public final String fullName;
 
     public Student(String dni, String fullName) {
+        this.uuid = UUID.randomUUID();
         this.dni = dni;
         this.fullName = fullName;
+    }
+
+    public UUID getUuid() {
+        return uuid;
     }
 }
